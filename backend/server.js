4ee15041371ae045ace0ee7fed2a9f48290e7b39
MitanshChauhan <mr.meetjay@gmail.com>
2026-03-24 08:06:45 +0530

    React URL added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // Allow React to connect
+    origin: "https://feed-hope-ten.vercel.app/"
     methods: ["GET", "POST", "PUT"]
   }
 });
